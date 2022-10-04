930f895b2008287197f3191b89a21d36c249491d
Aizawa <c0b21001be@edu.teu.ac.jp>
2022-10-04 15:12:20 +0900

    演習

--- ex02/test.py
+++ ex02/test.py
@@ -1,9 +1,29 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
 
-
+def click_number(event):
+    btn = event.widget
+    txt = btn["text"]
+    #tkm(txt,f"[{txt}]")
+    entry.insert(tk.END,txt)
 
 root= tk.Tk()
 root.title("")
 root.geometry("300x500")
+
+entry = tk.Entry(root, width=10, font=(", 40"),justify="right")
+entry.grid(row=0, column=0, columnspan=3)
+
+r, c = 1, 0
+numbers = list(range(9,-1,-1))
+operators = ["+"]
+for i, num in enumerate(numbers+operators, 1):
+    btn = tk.Button(root, text=f"{num}",font=("", 30), width=4, height=2)
+    btn.bind("<1>",click_number)
+    btn.grid(row = r, column=c)
+    c +=1
+    if i%3 == 0:
+        r +=1
+        c = 0
 
 root.mainloop()
