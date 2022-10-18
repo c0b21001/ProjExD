ceb799674c157359ccd8ad9e1a436b2a2f375bc1
Aizawa <c0b21001be@edu.teu.ac.jp>
2022-10-18 17:18:22 +0900

    追加

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,8 @@
+
 import tkinter as tk
+import tkinter.messagebox as tkm
 import maze_maker as mm #練習8
+
 
 
 def key_down(event):#練習5
@@ -10,21 +13,74 @@ def key_up(event):#練習6
     global key
     key = ""
 
+def dame():
+    global cx,cy
+    if maze_lst[my][mx] == 1:
+        tkm.showinfo("失敗", "✕とかべには入るな")
+        root.mainloop()
+
 def main_proc():
+    global tmr,jid
+    tmr = tmr+1 
+
+    global mx,my
     global cx, cy
+    global batu
+
+    
+    if mx ==13 and my ==7:                      #ゴールしたら
+        tkm.showinfo("goal", f"所要時間：{tmr/10}秒\nゴールしました。")
+        
+
+        root.mainloop()
+
+
     if key == "Up":
-        cy -= 20
+        maze_lst[my][mx] = 1
+        my -= 1
+        dame()
+        canv.create_image(cx, cy, image=batu, tag = "batu")
     if key == "Down":
-        cy += 20
+        maze_lst[my][mx] = 1
+        my += 1
+        dame()
+        canv.create_image(cx, cy, image=batu, tag = "batu")
     if key == "Right":
-        cx += 20
+        maze_lst[my][mx] = 1
+        mx += 1
+        dame()
+        canv.create_image(cx, cy, image=batu, tag = "batu")
     if key == "Left":
-        cx -= 20
+        maze_lst[my][mx] = 1
+        mx -= 1
+        dame()
+        canv.create_image(cx, cy, image=batu, tag = "batu")
+    if maze_lst[my][mx] == 0:          #床なら
+        cx, cy = mx*100+50, my*100+50
+    else:                              #壁なら
+        if key == "Up":
+            my += 1
+        if key == "Down":
+            my -= 1
+        if key == "Right":
+            mx -= 1
+        if key == "Left":
+            mx += 1
+    
+        
+
+    
+        
+
+
     canv.coords("tori",cx, cy)
     root.after(100,main_proc)
 
 
 if __name__ == "__main__":
+    tmr = 0
+
+
     root = tk.Tk()
     root.title("迷える工科トン")#練習1
 
@@ -32,10 +88,35 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack()
 
+    #練習9,10
+    maze_lst =  mm.make_maze(15, 9)
+    print(maze_lst)#１：壁0：床
+    mm.show_maze(canv, maze_lst)
+
+    #スタート
+    start1 = tk.PhotoImage(file="fig/start.png")
+    sx, sy = 1, 1
+    ssx, ssy = sx*100+50, sy*100+50
+    canv.create_image(ssx, ssy, image=start1, tag = "start")
+
+
+
+    #ゴール
+    goal = tk.PhotoImage(file="fig/goal.png")
+    gx, gy = 13, 7
+    ggx, ggy = gx*100+50, gy*100+50
+    canv.create_image(ggx, ggy, image=goal, tag = "goal")
+
+    #通った後
+    batu = tk.PhotoImage(file="fig/batu.png")
+
     #練習3
     tori = tk.PhotoImage(file="fig/3.png")
-    cx, cy = 300, 400
+    mx, my = 1, 1
+    cx, cy = mx*100+50, my*100+50
     canv.create_image(cx, cy, image=tori, tag = "tori")
+
+    
 
      #練習4
     key = "" #現在押されているキーを表す 
@@ -43,11 +124,10 @@ if __name__ == "__main__":
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
 
+    
 
     #練習7
     main_proc()
-    #練習9
-    maze_lst =  mm.make_maze(15, 9)
-    print(maze_lst)#１：壁0：床
-    mm.show_maze(canv, maze_lst)
+
+
     root.mainloop()
